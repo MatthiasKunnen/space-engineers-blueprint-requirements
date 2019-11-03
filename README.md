ff75a96aae35c7a2ec0b4d6d040e0b641411693c
Matthias Kunnen <matthias.kunnen@gmail.com>
2019-11-03 18:01:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Space engineers blueprint requirements
+This is an Angular web application that extracts the required components from a blueprint file
+*.sbc and allows exporting of this data for use in e.g. programmable blocks.
+
+Use the web application: <https://se-blueprint-requirements.herokuapp.com>.
+
+## Development
+Run using `yarn run start`. The website will be available on <http://localhost:4230>.
+
+## Contributing
+If you'd like to see a change or new feature, make an issue describing it or submit a PR. Creating
+an issue before submitting a PR increases acceptance rate for large changes.
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NotFoundComponent,
+        HomeComponent,
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule,
+        MatProgressSpinnerModule,
+        ReactiveFormsModule,
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {
+}
