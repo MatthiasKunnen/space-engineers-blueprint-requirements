24aa7e481c4b7ba908a318b10c760599293d4749
Matthias Kunnen <matthias.kunnen@gmail.com>
2019-11-03 21:53:38 +0000

    Add custom icon loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +8,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    constructor(
+        iconRegistry: MatIconRegistry,
+        sanitizer: DomSanitizer,
+    ) {
+        const addIcons: Array<{
+            external?: boolean,
+            name: string,
+            url: string,
+        }> = [
+        ];
+
+        addIcons.forEach(i => {
+            iconRegistry.addSvgIcon(
+                i.name,
+                sanitizer.bypassSecurityTrustResourceUrl(i.external === true
+                    ? i.url
+                    : `/assets/icons/${i.url}.svg`),
+            );
+        });
+    }
 }
